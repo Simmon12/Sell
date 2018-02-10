8975ad51f4b16cd047efd063b727d8b12bfb3e51
litchi <314749816@qq.com>
2018-02-10 22:17:53 +0800

    实现了header组件和导航栏和相应内容的滑动

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <v-header :seller="seller"></v-header>
+    </div>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods"> 商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <div class="content">
+      <router-view :seller="seller"></router-view>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import vHeader from '@/components/header/header'
+const ERR_OK = 0
+export default {
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    this.$ajax.get('/api/seller').then(res => {
+        console.log(res)
+        res = res.data
+        if(res.errno === ERR_OK) {
+          this.seller = res.data
+        }
+        console.log("seller",this.seller.name)
+    }).catch(err=> {
+      console.log(err)
+    })
+  },
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" >
+  #app
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+        .active
+          color: rgb(240,20,20)
+
+</style>
